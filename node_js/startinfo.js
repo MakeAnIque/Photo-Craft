a9b68f7681c57d337b06d5412df746d11441abf5
code-hunter <32864393+amitabh-anandcl@users.noreply.github.com>
2019-10-16 04:04:06 +0530

    file updated

---
+++ node_js/startinfo.js
@@ -0,0 +1,38 @@
+const chalk = require('chalk')
+const os = require('os')
+
+class stop_info {
+    constructor() {
+        let d = new Date()
+        let hrs = d.getHours()
+        let min = d.getMinutes()
+        let sec = d.getSeconds()
+
+        let PORT = 5000
+
+        const format = `[${hrs}:${min}:${sec}]`
+
+        let t = `
+            ${chalk.grey(format)} ${chalk.yellow(
+            'Server is Running on Port',
+        )} ${chalk.green(`[${PORT}]`)} ${chalk.red(
+            `platform ${os.platform()}`,
+        )} ${chalk.blue(`Arch ${os.arch()}`)}
+        `
+        let star = `
+              
+            ${chalk.grey(
+                '*********************************************************************',
+            )}
+                                                                            
+                ${t}                                                        
+                                                                             
+            ${chalk.grey(
+                '*********************************************************************',
+            )}
+        `
+        console.log(star)
+    }
+}
+
+const stop = new stop_info()
